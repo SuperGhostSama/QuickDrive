db4cb53e14575ddf73aa80d38365a9e0fba03caf
Salah <salaheddinehanaoui@gmail.com>
2023-03-08 16:35:35 +0100

    finished styling the reset password blade

--- BackEnd/resources/views/auth/reset-password.blade.php
+++ BackEnd/resources/views/auth/reset-password.blade.php
@@ -1,7 +1,9 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
-            <x-authentication-card-logo />
+            <a class="navbar-brand fw-bold text-white fs-1" href="index.html">QUICKDRIVE</a>
         </x-slot>
 
         <x-validation-errors class="mb-4" />
