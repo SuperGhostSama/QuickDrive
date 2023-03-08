1342d3121adc1b1ed315efad839d41463236672f
Salah <salaheddinehanaoui@gmail.com>
2023-03-08 15:11:12 +0100

    fixed navbar links

--- BackEnd/resources/views/welcome.blade.php
+++ BackEnd/resources/views/welcome.blade.php
@@ -15,14 +15,14 @@
         <!-- Navbar -->
         <nav class="navbar navbar-expand-lg " data-bs-theme="dark" style="background-color: #000000">
             <div class="container-fluid">
-                <a class="navbar-brand fw-bold" href="index.html">QUICKDRIVE</a>
+                <a class="navbar-brand fw-bold" href="{{url ('/')}}">QUICKDRIVE</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-4 text-center">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+                            <a class="nav-link active" aria-current="page" href="{{url ('/')}}">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="#aboutUs">Abouts Us</a>
