f1cd0a094fea12b3f0df5a5699359a05d9f0afe6
Salah <salaheddinehanaoui@gmail.com>
2023-04-12 12:05:35 +0000

    added form validation for contact us form

--- Final Project/resources/views/pages/home.blade.php
+++ Final Project/resources/views/pages/home.blade.php
@@ -121,20 +121,20 @@
                 {{-- <img class="col-sm-12 col-lg-6 p-5" src="{{ asset('img/maps.png') }}" alt="maps"> --}}
             <div class="col-sm-12 col-lg-6 p-5">
                 <h3>Contact Us</h3>
-                <form action="{{ route('contacts.store') }}" method="POST" id="form">
+                <form action="{{ route('contacts.store') }}" method="POST" id="form" data-parsley-validate>
                     @csrf
                     <div class="mb-3">
                       <label for="contactName" class="form-label">Name</label>
-                      <input type="text" name="fullname" class="form-control" id="contactName" aria-describedby="emailHelp" placeholder="Full Name" required>
+                      <input type="text" name="fullname" class="form-control" id="contactName" aria-describedby="emailHelp" placeholder="Full Name" required data-parsley-pattern="/^[a-zA-Z ]*$/">
                     </div>
                     <div class="mb-3">
                       <label for="contactEmail" class="form-label">Email address</label>
-                      <input type="email" name="email" class="form-control" id="contactEmail" aria-describedby="emailHelp" placeholder="email@mail.com" required>
+                      <input type="email" name="email" class="form-control" id="contactEmail" aria-describedby="emailHelp" placeholder="email@mail.com" required placeholder="enter email" data-parsley-pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$">
                     </div>
                     
                     <div class="mb-3">
                       <label for="contactComment" class="form-label">Comment</label>
-                      <textarea type="text" name="message" class="form-control" id="contactComment" placeholder="Leave a message here" required style="max-height: 269px; min-height: 269px;"></textarea>
+                      <textarea type="text" name="message" class="form-control" id="contactComment" placeholder="Leave a message here" data-parsley-pattern="^[A-Za-z0-9 ,\-_.]*$" required style="max-height: 269px; min-height: 269px;"></textarea>
                     </div>
                     
                     <button type="submit" name="send" class="btn-prim col-10 col-sm-2 text-white w-100 p-1">Send</button>
